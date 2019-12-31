ab3273a899cb58ce9524e8a453d4279bd74c8c4e
thuyvy3110 <dnthuyvy@gmail.com>
2020-01-01 06:59:43 +0700

    Create Hello world page

--- src/test/java/com/dawn/jat/illuminati/IlluminatiApplicationTests.java
+++ src/test/java/com/dawn/jat/illuminati/IlluminatiApplicationTests.java
@@ -1,11 +1,22 @@
 package com.dawn.jat.illuminati;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.assertj.core.api.Assertions.assertThat;
 
+import com.dawn.jat.illuminati.hello.HelloController;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class IlluminatiApplicationTests {
+    @Autowired
+    private HelloController controller;
+
     @Test
-    void contextLoads() {
+    void contextLoads() throws Exception {
+        assertThat(controller).isNotNull();
     }
 }
