4e58be58ee2d5af73d1296018297a7eb1e1d2c25
Phat Ho <hhphat@apcs.vn>
2019-12-31 22:33:36 +0700

    Edit Release script

--- script/release.sh
+++ script/release.sh
@@ -1,6 +1,5 @@
 set -e
-cd ..
-./gradlew generateGitChangelogMinimal
-git add CHANGELOG.md
+../gradlew generateGitChangelogMinimal
+git add ../CHANGELOG.md
 git commit -m "Add CHANGELOG to new Release"
-./gradlew release
+../gradlew release
