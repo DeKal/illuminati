cc252d09473011746edf42f7b7e1e07869f9d0d0
thuyvy3110 <dnthuyvy@gmail.com>
2020-01-01 21:43:41 +0700

    Add Unit test & Intergation test

--- src/test/java/com/dawn/jat/illuminati/IlluminatiApplicationTests.java
+++ src/test/java/com/dawn/jat/illuminati/IlluminatiApplicationTests.java
@@ -1,22 +1,12 @@
 package com.dawn.jat.illuminati;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
-import com.dawn.jat.illuminati.hello.HelloController;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
 class IlluminatiApplicationTests {
-    @Autowired
-    private HelloController controller;
-
     @Test
-    void contextLoads() throws Exception {
-        assertThat(controller).isNotNull();
+    void contextLoads() {
+        IlluminatiApplication.main(new String[]{""});
     }
 }
--- src/test/java/com/dawn/jat/illuminati/hello/HelloControllerTest.java
+++ src/test/java/com/dawn/jat/illuminati/hello/HelloControllerTest.java
@@ -1,27 +1,18 @@
 package com.dawn.jat.illuminati.hello;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class HelloControllerTest {
-    @LocalServerPort
-    private int port;
-
     @Autowired
-    private TestRestTemplate restTemplate;
+    private HelloController controller;
 
     @Test
-    public void helloPageShouldReturnDefaultMessage() throws Exception {
-        assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/hello",
-                String.class)).contains("Hello World!!!");
+    public void helloPage() {
+        assertEquals("hello/index", controller.index());
     }
 }
