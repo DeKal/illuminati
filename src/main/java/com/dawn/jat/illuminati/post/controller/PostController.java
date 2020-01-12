d4d14ca278cde0826461a9ebc6f8e27fe8bbb5e7
thuyvy3110 <dnthuyvy@gmail.com>
2020-01-12 23:16:44 +0700

    Updated Unit test, remove Integration test

--- src/main/java/com/dawn/jat/illuminati/post/controller/PostController.java
+++ src/main/java/com/dawn/jat/illuminati/post/controller/PostController.java
@@ -3,23 +3,31 @@ package com.dawn.jat.illuminati.post.controller;
 import com.dawn.jat.illuminati.post.entity.PostEntity;
 import com.dawn.jat.illuminati.post.service.PostService;
 
+import java.util.List;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
+@RequestMapping({"/post"})
 public class PostController {
     @Autowired
     PostService postService;
 
+    @GetMapping
+    public List<PostEntity> findAll() {
+        return postService.findAll();
+    }
+
     /**
      * Gets a Post by the Id.
      */
-    @RequestMapping(value = "/post/{id}")
+    @RequestMapping(value = "/{id}")
     public ResponseEntity<String> getPostById(@PathVariable("id") String id) {
         Optional<PostEntity> post = postService.findById(id);
 
--- src/main/java/com/dawn/jat/illuminati/post/service/PostService.java
+++ src/main/java/com/dawn/jat/illuminati/post/service/PostService.java
@@ -3,6 +3,7 @@ package com.dawn.jat.illuminati.post.service;
 import com.dawn.jat.illuminati.post.entity.PostEntity;
 import com.dawn.jat.illuminati.post.repository.PostRepository;
 
+import java.util.List;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,7 +15,19 @@ public class PostService {
     @Autowired
     private PostRepository postRepository;
 
+    public List<PostEntity> findAll() {
+        return postRepository.findAll();
+    }
+
     public Optional<PostEntity> findById(String id) {
         return postRepository.findById(id);
+    }
+
+    public PostEntity createOrUpdate(PostEntity postEntity) {
+        return postRepository.save(postEntity);
+    }
+
+    public void deleteById(String id) {
+        postRepository.deleteById(id);
     }
 }
