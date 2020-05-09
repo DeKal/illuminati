9c83c0b9f613b4c57fc2ca3186c14f99074dd154
Phat Ho <hhphat@apcs.vn>
2020-05-09 23:32:51 +0700

    [#28] Just fix line format

--- src/main/java/com/dawn/jat/illuminati/WebSecurityConfig.java
+++ src/main/java/com/dawn/jat/illuminati/WebSecurityConfig.java
@@ -16,8 +16,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http
-                .authorizeRequests()
+        http.authorizeRequests()
                 .antMatchers("/api").permitAll()
                 .anyRequest().authenticated()
                 .and()
